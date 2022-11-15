e9fb690e68e36ec2281f0376c32bff5e45e921a5
sourabh singh <sourabhruhal0999@gmail.com>
2022-11-15 10:35:45 +0000

    React first program

---
+++ my-app/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render( 
+    <>
+    <h1>List of movies:</h1>
+    <p>These are as follows:</p>
+    <ol>
+        <li>Tarzan</li>
+        <li>Bareilly ki Barfi</li>
+        <li>The maze runner</li>
+        <li>Chandigarh kare Ashiqui</li>
+        <li>Puspha</li>
+    </ol>
+    </>,
+    document.getElementById("root")
+);
+
+
+
